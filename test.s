f5b0bc488268da0876084df938b17128042efa33
Roman Maksimovich <r.a.maksimovich@gmail.com>
2026-04-06 12:18:15 +0800

    06 Apr 2026 (Mon): 12:18

--- test.s
+++ test.s
@@ -1,22 +1,19 @@
 .data
-hello_string: .ascii "abcdefgh"
-# hello_string_length: .word . - hello_string
+l1: .word 0x0f000000
+l2: .word 0xffffffff
 
 .text
-main:
-    lw $t1, hello_string($zero)
-    move $a0, $t0
-    jal printint
-    li $v0, 4
-    la $a0, hello_string
-    syscall
-    jal exit
+.globl __start
+__start:
+    lw $t0, l1
+    lw $t1, l2
+    and $t2, $t0, $t1
+    xori $t3, $t1, 0xffff
+    srl $t2, $t2, 12
 
-printint:
-    li $v0, 1 # The print integer syscall code
-    syscall
-    jr $ra
+    move $t0, $zero
+    lui $t0, 0x0f00
+    andi $t0, 0x0000
 
-exit:
     li $v0, 10
     syscall
