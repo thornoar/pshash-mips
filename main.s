02ba28b9817618ae8d4b56c6e2adb4f67cca498d
Roman Maksimovich <r.a.maksimovich@gmail.com>
2026-04-07 11:57:36 +0800

    07 Apr 2026 (Tue): 11:57

--- main.s
+++ main.s
@@ -12,6 +12,17 @@ sourceNumbers: .asciiz "1952074386"
 .text
 .globl __start
 
+# Initialize the big number at address $a0 with the value 0.
+# The counter and limit are stored in the arguments $a2 and $a3.
+big_init:
+    bgeu $a2, $a3, init_if_break # Check if all the digits have already been initialized
+    sw $zero, 0($a0) # Initialize the first digit
+    addi $a0, $a0, 4 # Moving to the next digit
+    addi $a2, $a2, 1 # Increasing the counter
+    j big_init # Recursive call
+init_if_break:
+    jr $ra # Leave the function
+
 # Copy the big integer stored at address $a0 to address $a1.
 # The counter is given in $a2. The limit is given in $a3.
 big_copy:
@@ -30,7 +41,7 @@ cpy_if_break:
 big_increment:
     lw $t0, 0($a0) # Load the first digit into $t0
     addi $t0, $t0, 1 # Increment the first digit
-    add $t1, $zero, $zero
+    move $t1, $zero
     lui $t1, 0x8000 # Put the value 2^31 into $t1
     bgeu $t0, $t1, inc_if_carryover # Check if there is carry-over
     sw $t0, 0($a1) # There is no carryover, so store the incremented digit
@@ -48,4 +59,26 @@ inc_if_carryover:
     j big_increment # Recursive call
 
 __start:
-    
+    li $v0, 9 # memory allocation syscall
+    li $a0, 256 # number of bytes to allocate
+    syscall
+    move $s0, $v0 # Save the address of the first number
+    li $v0, 9 # memory allocation syscall
+    syscall
+    move $s1, $v0 # Save the address of the second number
+
+    # Initialize the first number
+    move $a0, $s0
+    move $a2, $zero
+    li $a3, 64
+    jal big_init
+
+    # # Initialize the second number
+    # move $a0, $s1
+    # move $a2, $zero
+    # li $a3, 64
+    # jal big_init
+
+    # Exit
+    li $v0, 10
+    syscall
