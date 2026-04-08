c59efdca379beb8b18a85ccb74ccffe5b11fc9e3
Roman Maksimovich <r.a.maksimovich@gmail.com>
2026-04-08 20:43:25 +0800

    08 Apr 2026 (Wed): 20:43

--- main.s
+++ main.s
@@ -15,6 +15,7 @@ colon_str: .asciiz ":"
 .text
 .globl __start
 .globl test
+.globl test2
 
 # # Arguments:
 # #   $a0: address of the number to initialize
@@ -28,24 +29,26 @@ colon_str: .asciiz ":"
 #     move $a1, $zero # Setting all other digits to zero
 #     j big_init # Recursive call
 
+# Instruction to jump to the return address
+return:
+    jr $ra
+
 # Arguments:
 #   $a0: address of the source number
 #   $a1: address of the destination number
-#   $a3: digit counter
 big_copy:
-    li $t0, 64
-    bgeu $a3, $t0, return # Check if all the digits have already been copied
+    li $t1, 0xffffffff # Loading the terminal value
+copy_loop:
     lw $t0, 0($a0) # Load the current digit
     sw $t0, 0($a1) # Copy the current digit
-    addi $a0, $a0, 4 # Moving to the next digit
-    addi $a1, $a1, 4 # Moving to the next digit
-    addi $a3, $a3, 1 # Increasing the counter
-    j big_copy # Recursive call
+    beq $t0, $t1, return # Check if all the digits have already been copied
+    addi $a0, $a0, 4 # Move to the next digit
+    addi $a1, $a1, 4 # Move to the next digit
+    j copy_loop # Next iteration
 
 # Arguments:
 #   $a0: address of number to increment
 #   $a1: address of number to store the result to
-#   $a3: digit counter
 big_increment:
     lw $t0, 0($a0) # Load the first digit into $t0
     addi $t0, $t0, 1 # Increment the first digit
@@ -53,48 +56,60 @@ big_increment:
     lui $t1, 0x8000 # Put the value 2^31 into $t1
     bgeu $t0, $t1, inc_if_carryover # Check if there is carry-over
     sw $t0, 0($a1) # There is no carryover, so store the incremented digit
-    addi $a0, $a0, 4
+    addi $a0, $a0, 4 # Move to the next digit
     addi $a1, $a1, 4 # Move to the next digit
-    addi $a3, $a3, 1 # Increase the counter
     j big_copy # Copy the remaining digits
 inc_if_carryover:
     sw $zero, 0($a1) # The first digit is now zero
     addi $a0, $a0, 4
     addi $a1, $a1, 4 # Move to the next digit
-    addi $a3, $a3, 1 # Increase the counter
-    j big_increment # Recursive call
+    j big_increment # Increment
 
 # Arguments:
 #   $a0: address of the first summand
 #   $a1: address of the second summand
 #   $a2: address of the result
-#   $a3: digit counter
 big_sum:
-    li $t0, 64
-    bgeu $a3, $t0, return # Check if all the digits have already been copied
     lw $t0, 0($a0) # Load the first digit of $a0
     lw $t1, 0($a1) # Load the first digit of $a1
+    li $t2, 0xffffffff # Load the terminal value
+    beq $t1, $t2, sum_copy_rest_fst # Check if it's the end of $a1
+    beq $t0, $t2, sum_copy_rest_snd # Check if it's the end of $a0
     addu $t2, $t0, $t1 # Add the first digits, store them in $t2
-    lui $t3, 0x8000
+    xor $t3, $t3, $t3 # Set $t3 to zero
+    lui $t3, 0x8000 # Load the binary value 2^31 in $t3
     bgeu $t2, $t3, sum_if_carryover
-    sw $t2, 0($a2) # Store the sum of digits
-    addi $a0, $a0, 4
+    sw $t2, 0($a2) # No carryover, so store the sum of digits
+    addi $a0, $a0, 4 # Move to the next digit
     addi $a1, $a1, 4 # Move to the next digit
     addi $a2, $a2, 4 # Move to the next digit
-    addi $a3, $a3, 1 # Increase the counter
-    move $a1, $a2
-    j big_increment
-sum_if_carryover:
-    subu $t2, $t2, $t3
-    sw $t2, 0($a2) # Store the sum of digits
-    addi $a0, $a0, 4
-    addi $a1, $a1, 4 # Move to the next digit
-    addi $a2, $a2, 4 # Move to the next digit
-    addi $a3, $a3, 1 # Increase the counter
     j big_sum # Recursive call
-    
-return:
-    jr $ra
+sum_if_carryover:
+    subu $t2, $t2, $t3 # Subtract 2^31 from $t2
+    sw $t2, 0($a2) # Store the sum of digits
+    addi $a0, $a0, 4 # Move to the next digit
+    addi $a1, $a1, 4 # Move to the next digit
+    addi $a2, $a2, 4 # Move to the next digit
+    addi $sp, $sp, -4 # Allocate 4 bytes on the stack
+    sw $ra, 0($sp) # Store the return address
+    addi $sp, $sp, -4 # Allocate 4 bytes on the stack
+    sw $a0, 0($sp) # Store $a0 on the stack
+    addi $sp, $sp, -4 # Allocate 4 bytes on the stack
+    sw $a1, 0($sp) # Store $a1 on the stack
+    move $a1, $a0 # $a1 := $a0
+    jal big_increment # Increment the first summand due to overflow
+    lw $a1, 0($sp) # Load back $a1
+    addi $sp, $sp, 4 # Move the stack pointer up 
+    lw $a0, 0($sp) # Load back $a0
+    addi $sp, $sp, 4 # Move the stack pointer up
+    lw $ra, 0($sp) # Load back the return address
+    addi $sp, $sp, 4 # Move the stack pointer up
+    j big_sum # Recursive call
+sum_copy_rest_snd:
+    move $a0, $a1
+sum_copy_rest_fst:
+    move $a1, $a2
+    j big_copy
 
 # Arguments:
 #   $a0: number of words to allocate
@@ -140,14 +155,16 @@ print_break:
     syscall # Print the endline
     jr $ra
 
+
+
 __start:
     li $a0, 8 # number of bytes to allocate
-    li $a1, 50 # Initial value
+    li $a1, 0x4000000f # Initial value
     jal big_alloc
     move $s0, $v0 # Save the address of the first number
 
     li $a0, 8 # number of bytes to allocate
-    li $a1, 75 # Initial value
+    li $a1, 0x40000000 # Initial value
     jal big_alloc
     move $s1, $v0 # Save the address of the second number
 
@@ -156,23 +173,22 @@ __start:
     jal big_alloc
     move $s2, $v0 # Save the address of the third number
 
-test:
     move $a0, $s0
     jal big_print
 
     move $a0, $s1
     jal big_print
 
-    # # Calculate the sum
-    # move $a0, $s0
-    # move $a1, $s1
-    # move $a2, $s2
-    # move $a3, $zero
-    # jal big_sum
-    #
-    # lw $a0, 0($s2)
-    # li $v0, 1
-    # syscall
+test:
+    # Calculate the sum
+    move $a0, $s0
+    move $a1, $s1
+    move $a2, $s2
+    jal big_sum
+
+test2:
+    move $a0, $s2
+    jal big_print
 
     # Exit
     li $v0, 10
